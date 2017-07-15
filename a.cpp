8ba01985ccb0458cfa035f1461880a8b87c7fb9c
Kshitij2000arora <kshitij_a@ymail.com>
2017-07-15 14:00:39 +0530

    Create a.cpp

---
+++ a.cpp
@@ -0,0 +1,12 @@
+// New to GitHub
+
+#include<iosteam.h>
+
+void main()
+{
+   int x;
+   cout<<"Enter your number :"<<endl;
+   cin>>x;
+   cout<<"The square of the given number is "<<x*x<<endl;
+   
+}
